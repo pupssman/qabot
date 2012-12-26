d56dc1d71a68655148122807b30b05062aeb5a0c
pupssman <pupssman@yandex-team.ru>
2012-12-26 21:44:37 +0400

    initial setuptools script

---
+++ setup.py
@@ -0,0 +1,15 @@
+'''
+Created on Dec 26, 2012
+
+@author: pupssman
+'''
+
+import setuptools
+
+PACKAGE = 'qabot'
+VERSION = '0.1'
+
+setuptools.setup(
+    name=PACKAGE,
+    version=VERSION,
+    description='Code for managing QA killer robot')
